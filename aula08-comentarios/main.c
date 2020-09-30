3bb65b6429880a7a1b47dea840f3987ffe731af4
Gustavo Tadei <gustavo.tadei@hotmail.com>
2020-09-30 16:39:31 -0400

    aula 08 - concluida

---
+++ aula08-comentarios/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    //Comentando uma linha
+    int x = 10;
+    //x = 2 * x;
+    printf("x = %d\n", x);
+
+    //Comentando bloco
+
+    /*
+    int x = 10;
+    x = 2 * x;
+    printf("x = %d\n", x);
+    */
+
+    system("pause");
+    return 0;
+}
