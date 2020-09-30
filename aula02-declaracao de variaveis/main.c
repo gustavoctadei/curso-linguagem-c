0500d6c86d7b7d244e61c34dbf5995738ce38ab2
Gustavo Tadei <gustavo.tadei@hotmail.com>
2020-09-30 09:29:16 -0400

    aula 02 - concluida | pastas renomeadas

---
+++ aula02-declaracao de variaveis/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    //char letra = 'a';
+    //char nro = 10;
+
+    //int nro = 10;
+    //float n = 5.25;
+    //double d = 5.0e3;
+
+    //int nro1 = 10;
+    //int nro2;
+    //int nro3;
+
+    int nro1 = 10, nro2, nro3;
+
+    system("pause");
+    return 0;
+}
