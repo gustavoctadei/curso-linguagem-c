bb682bc2b23ceb5e44aada11e909cbbc77daf2b4
Gustavo Tadei <gustavo.tadei@hotmail.com>
2020-09-30 16:05:00 -0400

    aula 06 - concluida

---
+++ aula06-constantes/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define PI 3.1415
+
+int main(){
+/*
+    int nro = 10;
+
+    const int nro = 10;
+*/
+
+///////////////////////////////////////
+
+/*
+    const int nro = 10;
+    int soma = nro + 1;
+
+    printf("Constante %d\n", nro);
+
+    //nro = 12;
+*/
+
+///////////////////////////////////////
+
+//Trabalhando com o define
+    float f = PI + 1;
+    printf("PI = %f\n", PI);
+
+    //PI = 12.4;
+
+    system("pause");
+    return 0;
+}
