162000dbf119076b62e0e3e445904b97c61a66c1
Gustavo Tadei <gustavo.tadei@hotmail.com>
2020-09-30 17:05:41 -0400

    aula 10 - concluida

---
+++ aula10-atribuicao simplificada/main.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+/*
+    //Soma simplificada
+    int x = 10, y = 10;
+    int z = 2;
+
+    x = x + z;
+    printf("x = %d\n", x);
+
+    y += z; //y = y + z
+    printf("y = %d\n", y);
+*/
+
+///////////////////////////////////////
+
+/*
+    //Subtracao simplificada
+    int x = 10, y = 10;
+    int z = 2;
+
+    x = x - z;
+    printf("x = %d\n", x);
+
+    y -= z; //y = y - z
+    printf("y = %d\n", y);
+*/
+
+///////////////////////////////////////
+
+/*
+    //Produto simplificado
+    int x = 10, y = 10;
+    int z = 2;
+
+    x = x * z;
+    printf("x = %d\n", x);
+
+    y *= z; //y = y * z
+    printf("y = %d\n", y);
+*/
+
+////////////////////////////////////////
+
+/*
+    //Divisao Simplificada
+    int x = 10, y = 10;
+    int z = 2;
+
+    x = x / z;
+    printf("x = %d\n", x);
+
+    y /= z; //y = y / z
+    printf("y = %d\n", y);
+*/
+
+////////////////////////////////////////
+
+    //Resto da divisao simplificado
+    int x = 10, y = 10;
+    int z = 2;
+
+    x = x % z;
+    printf("x = %d\n", x);
+
+    y %= z; //y = y % z
+    printf("y = %d\n", y);
+
+    system("pause");
+    return 0;
+}
